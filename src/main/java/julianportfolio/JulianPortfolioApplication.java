27aa8ca924a5a8acbbe5a1d35d3d4a356060827e
Julian Rich <julian_rich@hotmail.com>
2022-01-07 00:47:06 +1100

    Initial commit

---
+++ src/main/java/julianportfolio/JulianPortfolioApplication.java
@@ -0,0 +1,13 @@
+package julianportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulianPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulianPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/julianportfolio/JulianPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package julianportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulianPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
