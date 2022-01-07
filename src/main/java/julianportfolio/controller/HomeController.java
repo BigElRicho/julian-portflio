dcbfc582156b1076c73fe57460d69de847b46078
Julian Rich <julian_rich@hotmail.com>
2022-01-08 07:47:11 +1100

    added a random get method

--- src/main/java/julianportfolio/controller/HomeController.java
+++ src/main/java/julianportfolio/controller/HomeController.java
@@ -1,8 +1,21 @@
 package julianportfolio.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("/Portfolio")
 public class HomeController {
 
+    //Context etc. may go here.
+
+    public HomeController(){
+
+    }
+
+    @GetMapping("/getStuff")
+    public void getStuff(){
+        System.out.println("It works.");
+    }
 }
